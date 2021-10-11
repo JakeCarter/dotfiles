2ff25f9bb363e825349881a16ccb33fc3e1781aa
Jake Carter <jake@cartermade.com>
2021-10-10 18:25:14 -0700

    Added new `show-colors` function.

--- home/zsh/personal.zsh
+++ home/zsh/personal.zsh
@@ -2,10 +2,32 @@ ff () {
     find . \( \! -name .git -o -prune \) -a -type f -print0 | xargs -0 grep $*
 }
 
-show-colors() {
+list-colors() {
     for i in {0..255} ; do
         printf "\x1b[38;5;${i}m${i} "
     done
+}
+
+
+show-colors() {
+    # https://tldp.org/HOWTO/Bash-Prompt-HOWTO/x329.html
+    T='•••'   # The text for the color test
+    
+    echo -e "\n         def     40m     41m     42m     43m     44m     45m     46m     47m";
+    
+    for FGs in '    m' '   1m' '  30m' '1;90m' '  31m' '1;91m' '  32m' \
+               '1;92m' '  33m' '1;93m' '  34m' '1;94m' '  35m' '1;95m' \
+               '  36m' '1;96m' '  37m' '1;97m';
+    
+      do FG=${FGs// /}
+      echo -en " $FGs \033[$FG  $T  "
+      
+      for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
+        do echo -en "$EINS \033[$FG\033[$BG  $T  \033[0m";
+      done
+      echo;
+    done
+    echo
 }
 
 add-auth-tid() {
