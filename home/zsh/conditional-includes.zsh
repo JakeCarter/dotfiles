860a95784c1150146dc63cb0b307828e201d0a65
Jake Carter <jake@cartermade.com>
2023-08-13 11:17:03 -0700

    Add chruby/pyenv conditional config

--- home/zsh/conditional-includes.zsh
+++ home/zsh/conditional-includes.zsh
@@ -32,5 +32,16 @@ else
     echo '  brew install z; source ~/.zshrc'
 fi;
 
+# chruby - Ruby environment tool
+[[ -f /opt/homebrew/opt/chruby/share/chruby/chruby.sh ]] && source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
+[[ -f /opt/homebrew/opt/chruby/share/chruby/auto.sh ]] && source /opt/homebrew/opt/chruby/share/chruby/auto.sh
+[[ -f /opt/homebrew/opt/chruby/share/chruby/chruby.sh ]] && chruby ruby-3.1.2
+
+# pyenv - Python version management
+if command -v pyenv 1>/dev/null 2>&1; then
+    export PATH=~/.pyenv/bin:$PATH
+    eval "$(pyenv init -)"
+fi
+
 # work.zsh
 [[ -f ~/.work/work.zsh ]] && source ~/.work/work.zsh
