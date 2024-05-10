5d94ce3196cfd965a391e93b57d85c1ff2cf92c9
Jake Carter <jake@cartermade.com>
2024-05-10 15:00:50 -0700

    Added zq and xp aliases

--- home/zsh/aliases.zsh
+++ home/zsh/aliases.zsh
@@ -2,6 +2,13 @@ alias ls='ls -G'
 alias la='ls -lah'
 alias gs='git status'
 alias a86='arch -x86_64'
+alias zq='zoxide query'
+
+# Alias for finding the path to the currently seleted Xcode's app root path
+# Assuming Xcode is installed at "/Applications/Xcode.app"
+#   `xcode-select -p` would return "/Applications/Xcode.app/Contents/Develooper"
+#   `xp` would return "/Applications/Xcode.app"
+alias xp='xcode-select -p | xargs dirname | xargs dirname'
 
 alias ds='dirs -v'
 # Create aliases 1-9 for hopping around the dir stack (i.e. alias 1='cd +1')
