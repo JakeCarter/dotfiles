9163947733ca10f415e0860fe52fff5a8fbd5084
Jake Carter <jake@cartermade.com>
2026-04-12 08:42:21 -0700

    Fixed aerospace helper script to account for vim bindings

--- stow/scripts/dot-scripts/aerospace-next-free-workspace.zsh
+++ stow/scripts/dot-scripts/aerospace-next-free-workspace.zsh
@@ -2,8 +2,8 @@
 
 active=($(aerospace list-workspaces --all))
 
-# Skips 0, which I treat as special and always bound to my laptop display, and my aerospace arrow keys - I, J, K and L
-candidates=({1..9} {A..H} {M..Z})
+# Skips 0, which I treat as special and always bound to my laptop display, and my aerospace arrow keys - H, J, K and L
+candidates=({1..9} {A..G} I {M..Z})
 
 for candidate in $candidates; do
   if (( ! ${active[(Ie)$candidate]} )); then
